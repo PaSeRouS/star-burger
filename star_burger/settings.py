161cb1a65815f517508ef859cb7d9c84c231c630
PaSeRouS <paser96@mail.ru>
2022-09-25 18:08:01 +0300

    Исправление ошибок при развёртывании проекта

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -38,6 +38,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'foodcartapp.middleware.AjaxMiddleware',
 ]
 
 ROOT_URLCONF = 'star_burger.urls'
